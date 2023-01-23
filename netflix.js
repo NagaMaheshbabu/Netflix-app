02b678dfcffc3c1026c45d07cc08765e66f52827
NagaMaheshbabu <87943821+NagaMaheshbabu@users.noreply.github.com>
2023-01-23 18:42:26 +0530

    netflix-update

---
+++ netflix.js
@@ -0,0 +1,15 @@
+
+var bulb = document.getElementById("bulb");
+
+bulb.onclick = function () {
+    if (bulb.style.color = "white") {
+        window.open("index1.html")
+
+    }
+    else {
+        window.open("index.html")
+    }
+}
+
+
+int 
